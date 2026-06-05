0ccea6f932c1fb3737ec20ea2a7aa88c50b4c2c7
marcoviniciusmx <marco10091010@gmail.com>
2026-06-05 11:28:49 -0300

    feat: connect backend to postgres database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express'
 import cors from 'cors'
+import { pool } from './config/database.js'
 
 const PORT = process.env.PORT || 3004
 
@@ -17,6 +18,16 @@ app.get('/health', (req, res) => {
     res.send({
         'status': 'ok',
         'message': 'API OrçaFácil Lite funcionando'
+    })
+})
+
+app.get('/db-test', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+
+    res.json({
+        status: 'ok',
+        message: 'Banco conectado com sucesso',
+        databaseTime: result.rows[0].now
     })
 })
 
